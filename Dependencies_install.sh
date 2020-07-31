4999716eddbaaee1722f07cb668886dbfe28ccad
biyuehuang <31006098+biyuehuang@users.noreply.github.com>
2020-07-31 16:12:21 +0800

    Create Dependencies_install.sh

---
+++ Dependencies_install.sh
@@ -0,0 +1,56 @@
+echo
+echo "Dependencies installation Start"
+echo
+
+#python3 -m pip install testresources
+
+echo
+echo ! Start Annotation Tool Environment Initialization
+echo
+
+cur_path=$PWD
+cd Annotation_Tool/labelImg/
+apt-get install -y pyqt5-dev-tools
+#python3 -m pip3 install -r requirements/requirements-linux-python3.txt
+#sudo pip3 install -r requirements/requirements-linux-python3.txt
+python3 -m pip install -r requirements/requirements-linux-python3.txt
+make qt5py3
+cd $cur_path
+
+echo
+echo ! Start Tensorflow_models Environment Initialization
+echo
+
+apt-get install -y protobuf-compiler python-pil python-lxml python-tk
+#pip3 install Cython
+#pip3 install contextlib2
+
+#CURRENT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+
+tf_research_dir="$cur_path/Tensorflow_models/research"
+cd $tf_research_dir
+protoc object_detection/protos/*.proto --python_out=.
+
+sudo apt-get install unzip
+unzip -o protobuf.zip
+./bin/protoc object_detection/protos/*.proto --python_out=.
+export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
+
+tf_slim_path="$cur_path/Tensorflow_models/research/slim/"
+#cd $tf_slim_path
+#wget https://pypi.python.org/packages/source/s/setuptools/setuptools-0.6c11.tar.gz
+#tar zxvf setuptools-0.6c11.tar.gz
+#cd setuptools-0.6c11
+#sudo python3 setup.py install
+cd $tf_slim_path
+python3 setup.py build   
+sudo python3 setup.py install
+
+cd ../../..
+
+echo
+echo ! Finished Quick Start Demo Environment Initialization
+echo
+
+### Use Visualization Demo need to install tensorflow
